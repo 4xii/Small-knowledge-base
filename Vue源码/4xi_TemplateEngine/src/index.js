2381c556e884fd77cc7dde316f9f1e5680f562a4
4xi <975795875@qq.com>
2021-02-03 17:23:59 +0800

    tokens生成一维

--- Vue源码/4xi_TemplateEngine/src/index.js
+++ Vue源码/4xi_TemplateEngine/src/index.js
@@ -1,5 +1,10 @@
+import parseTemplateToTokens from './parseTemplateToTokens.js'
+//全局提供Sxi_TemplateEngine对象
 window.Sxi_TemplateEngine = {
-  render(){
-    console.log(123);
+  //渲染方法
+  render(templateStr,data){
+    //调用parseTemplateToTokens函数，让模板字符串能够变为tokens数组
+    var tokens = parseTemplateToTokens(templateStr);
+    console.log(tokens)
   }
 }
---
+++ Vue源码/4xi_TemplateEngine/src/parseTemplateToTokens.js
@@ -0,0 +1,40 @@
+import Scanner from './Scanner.js'
+
+/* 
+  将模板字符串变为tokens数组
+*/
+export default function parseTemplateToTokens(templateStr) {
+  var tokens = [];
+  //创建扫描器
+  var scanner = new Scanner(templateStr);
+  var words;
+  //扫描器工作
+  while(!scanner.eos()){
+    //收集开始标记出现之前的文字
+    words = scanner.scanUtil('{{');
+    if(words != ''){
+      //存text
+      tokens.push(['text',words]);
+    }    
+    //过双大括号
+    scanner.scan('{{');
+    //收集name
+    words = scanner.scanUtil('}}');
+    if(words != ''){
+      //这个words就是{{}}中间的东西，判断一下首字符
+      if(words[0] == '#'){
+        //存起来,从下标为1的项开始存，下标为0的项是#
+        tokens.push(['#',words.substring(1)]);
+      } else if (word[0] == '/'){
+        //存起来,从下标为1的项开始存，下标为0的项是/
+        tokens.push(['/',words.substring(1)]);
+      } else {
+        //存起来
+        tokens.push(['text',words]);
+      }
+    }
+    //过双大括号
+    scanner.scan('}}');
+  }
+  return tokens;
+}
