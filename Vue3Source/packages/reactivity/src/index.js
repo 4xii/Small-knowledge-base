3f32caab340d2461e57e2fa2ef59e7ba43f86f22
4xi <975795875@qq.com>
2021-11-17 07:21:09 +0800

    .

--- Vue3Source/packages/reactivity/src/index.js
+++
@@ -1,9 +0,0 @@
-//导出方法，不实现功能
-import {
-    reactive,
-    shallowReactive,
-    shallowReadonly,
-    readonly
-} from './reactive'
-let state = reactive({ name: '4xi', age: { n: 12 } })
-console.log(state.age);
--- Vue3Source/packages/shared/dist/shared.esm-bundler.js
+++
@@ -1,5 +0,0 @@
-const isObject = (value) => typeof value == 'object' && value !== null;
-const extend = Object.assign;
-
-export { extend, isObject };
-//# sourceMappingURL=shared.esm-bundler.js.map
